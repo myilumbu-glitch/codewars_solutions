4696261710c000d36fa0d3aff109a136d4503a84
myilumbu-glitch <2.myilumbu@gmail.com>
2026-02-17 04:49:45 -0800

    vowel_count.py

---
+++ vowel_count.py
@@ -0,0 +1,15 @@
+#Return the number (count) of vowels in the given string.
+#consider a, e, i, o, u as vowels for this Kata (but not y).
+#The input string will only consist of lower case letters and/or spaces.
+
+# Codewars Kata: Vowel Count
+# Difficulty: 7 kyu (or whatever level it was)
+
+def get_count(vowels):
+    count = 0
+    
+    for x in vowels:
+        if x in "aeiou":
+            count += 1
+            
+    return count
